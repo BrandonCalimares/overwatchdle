8ac0b9e2666741debcd6f874ef640d1882214458
Brandon Calimares <brandoncalimares@gmail.com>
2024-09-09 23:42:30 -0300

    Agregando todas las imagenes de las habilidades y archivo abilities.js

---
+++ js/abilities.js
@@ -0,0 +1,728 @@
+const abilities = [
+    // Ana
+    {
+        hero: 'ana',
+        key: 'e',
+        img: 'imgs/abilities/ana-e.png',
+    },
+    {
+        hero: 'ana',
+        key: 'q',
+        img: 'imgs/abilities/ana-q.png',
+    },
+    {
+        hero: 'ana',
+        key: 'shift',
+        img: 'imgs/abilities/ana-shift.png',
+    },
+    // Ashe
+    {
+        hero: 'ashe',
+        key: 'e',
+        img: 'imgs/abilities/ashe-e.png',
+    },
+    {
+        hero: 'ashe',
+        key: 'q',
+        img: 'imgs/abilities/ashe-q.png',
+    },
+    {
+        hero: 'ashe',
+        key: 'shift',
+        img: 'imgs/abilities/ashe-shift.png',
+    },
+    // Baptiste
+    {
+        hero: 'baptiste',
+        key: 'e',
+        img: 'imgs/abilities/baptiste-e.png',
+    },
+    {
+        hero: 'baptiste',
+        key: 'q',
+        img: 'imgs/abilities/baptiste-q.png',
+    },
+    {
+        hero: 'baptiste',
+        key: 'shift',
+        img: 'imgs/abilities/baptiste-shift.png',
+    },
+    // Bastion
+    {
+        hero: 'bastion',
+        key: 'e',
+        img: 'imgs/abilities/bastion-e.png',
+    },
+    {
+        hero: 'bastion',
+        key: 'q',
+        img: 'imgs/abilities/bastion-q.png',
+    },
+    {
+        hero: 'bastion',
+        key: 'shift',
+        img: 'imgs/abilities/bastion-shift.png',
+    },
+    {
+        hero: 'bastion',
+        key: 'right-click',
+        img: 'imgs/abilities/bastion-rc.png',
+    },
+    // Brigitte
+    {
+        hero: 'brigitte',
+        key: 'e',
+        img: 'imgs/abilities/brigitte-e.png',
+    },
+    {
+        hero: 'brigitte',
+        key: 'q',
+        img: 'imgs/abilities/brigitte-q.png',
+    },
+    {
+        hero: 'brigitte',
+        key: 'shift',
+        img: 'imgs/abilities/brigitte-shift.png',
+    },
+    {
+        hero: 'brigitte',
+        key: 'right-click',
+        img: 'imgs/abilities/brigitte-rc.png',
+    },
+    // Cassidy
+    {
+        hero: 'cassidy',
+        key: 'q',
+        img: 'imgs/abilities/cassidy-q.png',
+    },
+    {
+        hero: 'cassidy',
+        key: 'shift',
+        img: 'imgs/abilities/cassidy-shift.png',
+    },
+    // Doomfist
+    {
+        hero: 'doomfist',
+        key: 'e',
+        img: 'imgs/abilities/doomfist-e.png',
+    },
+    {
+        hero: 'doomfist',
+        key: 'q',
+        img: 'imgs/abilities/doomfist-q.png',
+    },
+    {
+        hero: 'doomfist',
+        key: 'shift',
+        img: 'imgs/abilities/doomfist-shift.png',
+    },
+    {
+        hero: 'doomfist',
+        key: 'right-click',
+        img: 'imgs/abilities/doomfist-rc.png',
+    },
+    // D.Va
+    {
+        hero: 'dva',
+        key: 'e',
+        img: 'imgs/abilities/dva-e.png',
+    },
+    {
+        hero: 'dva',
+        key: 'q',
+        img: 'imgs/abilities/dva-q.png',
+    },
+    {
+        hero: 'dva',
+        key: 'shift',
+        img: 'imgs/abilities/dva-shift.png',
+    },
+    {
+        hero: 'dva',
+        key: 'right-click',
+        img: 'imgs/abilities/dva-rc.png',
+    },
+    // Echo
+    {
+        hero: 'echo',
+        key: 'e',
+        img: 'imgs/abilities/echo-e.png',
+    },
+    {
+        hero: 'echo',
+        key: 'q',
+        img: 'imgs/abilities/echo-q.png',
+    },
+    {
+        hero: 'echo',
+        key: 'shift',
+        img: 'imgs/abilities/echo-shift.png',
+    },
+    {
+        hero: 'echo',
+        key: 'right-click',
+        img: 'imgs/abilities/echo-rc.png',
+    },
+    // Genji
+    {
+        hero: 'genji',
+        key: 'e',
+        img: 'imgs/abilities/genji-e.png',
+    },
+    {
+        hero: 'genji',
+        key: 'q',
+        img: 'imgs/abilities/genji-q.png',
+    },
+    {
+        hero: 'genji',
+        key: 'shift',
+        img: 'imgs/abilities/genji-shift.png',
+    },
+    // Hanzo
+    {
+        hero: 'hanzo',
+        key: 'e',
+        img: 'imgs/abilities/hanzo-e.png',
+    },
+    {
+        hero: 'hanzo',
+        key: 'q',
+        img: 'imgs/abilities/hanzo-q.png',
+    },
+    {
+        hero: 'hanzo',
+        key: 'shift',
+        img: 'imgs/abilities/hanzo-shift.png',
+    },
+    // Illari
+    {
+        hero: 'illari',
+        key: 'e',
+        img: 'imgs/abilities/illari-e.png',
+    },
+    {
+        hero: 'illari',
+        key: 'q',
+        img: 'imgs/abilities/illari-q.png',
+    },
+    {
+        hero: 'illari',
+        key: 'shift',
+        img: 'imgs/abilities/illari-shift.png',
+    },
+    // Junkerqueen
+    {
+        hero: 'junkerqueen',
+        key: 'e',
+        img: 'imgs/abilities/junkerqueen-e.png',
+    },
+    {
+        hero: 'junkerqueen',
+        key: 'q',
+        img: 'imgs/abilities/junkerqueen-q.png',
+    },
+    {
+        hero: 'junkerqueen',
+        key: 'shift',
+        img: 'imgs/abilities/junkerqueen-shift.png',
+    },
+    {
+        hero: 'junkerqueen',
+        key: 'right-click',
+        img: 'imgs/abilities/junkerqueen-rc.png',
+    },
+    // Junkrat
+    {
+        hero: 'junkrat',
+        key: 'e',
+        img: 'imgs/abilities/junkrat-e.png',
+    },
+    {
+        hero: 'junkrat',
+        key: 'q',
+        img: 'imgs/abilities/junkrat-q.png',
+    },
+    {
+        hero: 'junkrat',
+        key: 'shift',
+        img: 'imgs/abilities/junkrat-shift.png',
+    },
+    // Juno
+    {
+        hero: 'juno',
+        key: 'e',
+        img: 'imgs/abilities/juno-e.png',
+    },
+    {
+        hero: 'juno',
+        key: 'q',
+        img: 'imgs/abilities/juno-q.png',
+    },
+    {
+        hero: 'juno',
+        key: 'shift',
+        img: 'imgs/abilities/juno-shift.png',
+    },
+    {
+        hero: 'juno',
+        key: 'right-click',
+        img: 'imgs/abilities/juno-rc.png',
+    },
+    // Kiriko
+    {
+        hero: 'kiriko',
+        key: 'e',
+        img: 'imgs/abilities/kiriko-e.png',
+    },
+    {
+        hero: 'kiriko',
+        key: 'q',
+        img: 'imgs/abilities/kiriko-q.png',
+    },
+    {
+        hero: 'kiriko',
+        key: 'shift',
+        img: 'imgs/abilities/kiriko-shift.png',
+    },
+    // Lifeweaver
+    {
+        hero: 'lifeweaver',
+        key: 'e',
+        img: 'imgs/abilities/lifeweaver-e.png',
+    },
+    {
+        hero: 'lifeweaver',
+        key: 'q',
+        img: 'imgs/abilities/lifeweaver-q.png',
+    },
+    {
+        hero: 'lifeweaver',
+        key: 'shift',
+        img: 'imgs/abilities/lifeweaver-shift.png',
+    },
+    // Lucio
+    {
+        hero: 'lucio',
+        key: 'e',
+        img: 'imgs/abilities/lucio-e.png',
+    },
+    {
+        hero: 'lucio',
+        key: 'q',
+        img: 'imgs/abilities/lucio-q.png',
+    },
+    {
+        hero: 'lucio',
+        key: 'shift',
+        img: 'imgs/abilities/lucio-shift.png',
+    },
+    {
+        hero: 'lucio',
+        key: 'right-click',
+        img: 'imgs/abilities/lucio-rc.png',
+    },
+    // Mauga
+    {
+        hero: 'mauga',
+        key: 'e',
+        img: 'imgs/abilities/mauga-e.png',
+    },
+    {
+        hero: 'mauga',
+        key: 'q',
+        img: 'imgs/abilities/mauga-q.png',
+    },
+    {
+        hero: 'mauga',
+        key: 'shift',
+        img: 'imgs/abilities/mauga-shift.png',
+    },
+    // Mei
+    {
+        hero: 'mei',
+        key: 'e',
+        img: 'imgs/abilities/mei-e.png',
+    },
+    {
+        hero: 'mei',
+        key: 'q',
+        img: 'imgs/abilities/mei-q.png',
+    },
+    {
+        hero: 'mei',
+        key: 'shift',
+        img: 'imgs/abilities/mei-shift.png',
+    },
+    // Mercy
+    {
+        hero: 'mercy',
+        key: 'e',
+        img: 'imgs/abilities/mercy-e.png',
+    },
+    {
+        hero: 'mercy',
+        key: 'q',
+        img: 'imgs/abilities/mercy-q.png',
+    },
+    {
+        hero: 'mercy',
+        key: 'shift',
+        img: 'imgs/abilities/mercy-shift.png',
+    },
+    // Moira
+    {
+        hero: 'moira',
+        key: 'e',
+        img: 'imgs/abilities/moira-e.png',
+    },
+    {
+        hero: 'moira',
+        key: 'q',
+        img: 'imgs/abilities/moira-q.png',
+    },
+    {
+        hero: 'moira',
+        key: 'shift',
+        img: 'imgs/abilities/moira-shift.png',
+    },
+    // Orisa
+    {
+        hero: 'orisa',
+        key: 'e',
+        img: 'imgs/abilities/orisa-e.png',
+    },
+    {
+        hero: 'orisa',
+        key: 'q',
+        img: 'imgs/abilities/orisa-q.png',
+    },
+    {
+        hero: 'orisa',
+        key: 'shift',
+        img: 'imgs/abilities/orisa-shift.png',
+    },
+    {
+        hero: 'orisa',
+        key: 'right-click',
+        img: 'imgs/abilities/orisa-rc.png',
+    },
+    // Pharah
+    {
+        hero: 'pharah',
+        key: 'e',
+        img: 'imgs/abilities/pharah-e.png',
+    },
+    {
+        hero: 'pharah',
+        key: 'q',
+        img: 'imgs/abilities/pharah-q.png',
+    },
+    {
+        hero: 'pharah',
+        key: 'shift',
+        img: 'imgs/abilities/pharah-shift.png',
+    },
+    // Ramattra
+    {
+        hero: 'ramattra',
+        key: 'e',
+        img: 'imgs/abilities/ramattra-e.png',
+    },
+    {
+        hero: 'ramattra',
+        key: 'q',
+        img: 'imgs/abilities/ramattra-q.png',
+    },
+    {
+        hero: 'ramattra',
+        key: 'right-click',
+        img: 'imgs/abilities/ramattra-rc.png',
+    },
+    // Reaper
+    {
+        hero: 'reaper',
+        key: 'e',
+        img: 'imgs/abilities/reaper-e.png',
+    },
+    {
+        hero: 'reaper',
+        key: 'q',
+        img: 'imgs/abilities/reaper-q.png',
+    },
+    {
+        hero: 'reaper',
+        key: 'shift',
+        img: 'imgs/abilities/reaper-shift.png',
+    },
+    // Reinhardt
+    {
+        hero: 'reinhardt',
+        key: 'e',
+        img: 'imgs/abilities/reinhardt-e.png',
+    },
+    {
+        hero: 'reinhardt',
+        key: 'q',
+        img: 'imgs/abilities/reinhardt-q.png',
+    },
+    {
+        hero: 'reinhardt',
+        key: 'shift',
+        img: 'imgs/abilities/reinhardt-shift.png',
+    },
+    {
+        hero: 'reinhardt',
+        key: 'right-click',
+        img: 'imgs/abilities/reinhardt-rc.png',
+    },
+    // Roadhog
+    {
+        hero: 'roadhog',
+        key: 'e',
+        img: 'imgs/abilities/roadhog-e.png',
+    },
+    {
+        hero: 'roadhog',
+        key: 'q',
+        img: 'imgs/abilities/roadhog-q.png',
+    },
+    {
+        hero: 'roadhog',
+        key: 'shift',
+        img: 'imgs/abilities/roadhog-shift.png',
+    },
+    {
+        hero: 'roadhog',
+        key: 'right-click',
+        img: 'imgs/abilities/roadhog-rc.png',
+    },
+    // Sigma
+    {
+        hero: 'sigma',
+        key: 'e',
+        img: 'imgs/abilities/sigma-e.png',
+    },
+    {
+        hero: 'sigma',
+        key: 'q',
+        img: 'imgs/abilities/sigma-q.png',
+    },
+    {
+        hero: 'sigma',
+        key: 'shift',
+        img: 'imgs/abilities/sigma-shift.png',
+    },
+    {
+        hero: 'sigma',
+        key: 'right-click',
+        img: 'imgs/abilities/sigma-rc.png',
+    },
+    // Sojourn
+    {
+        hero: 'sojourn',
+        key: 'e',
+        img: 'imgs/abilities/sojourn-e.png',
+    },
+    {
+        hero: 'sojourn',
+        key: 'q',
+        img: 'imgs/abilities/sojourn-q.png',
+    },
+    {
+        hero: 'sojourn',
+        key: 'shift',
+        img: 'imgs/abilities/sojourn-shift.png',
+    },
+    // Soldier76
+    {
+        hero: 'soldier76',
+        key: 'e',
+        img: 'imgs/abilities/soldier76-e.png',
+    },
+    {
+        hero: 'soldier76',
+        key: 'q',
+        img: 'imgs/abilities/soldier76-q.png',
+    },
+    {
+        hero: 'soldier76',
+        key: 'shift',
+        img: 'imgs/abilities/soldier76-shift.png',
+    },
+    {
+        hero: 'soldier76',
+        key: 'right-click',
+        img: 'imgs/abilities/soldier76-rc.png',
+    },
+    // Sombra
+    {
+        hero: 'sombra',
+        key: 'e',
+        img: 'imgs/abilities/sombra-e.png',
+    },
+    {
+        hero: 'sombra',
+        key: 'q',
+        img: 'imgs/abilities/sombra-q.png',
+    },
+    {
+        hero: 'sombra',
+        key: 'shift',
+        img: 'imgs/abilities/sombra-shift.png',
+    },
+    {
+        hero: 'sombra',
+        key: 'right-click',
+        img: 'imgs/abilities/sombra-rc.png',
+    },
+    // Symmetra
+    {
+        hero: 'symmetra',
+        key: 'e',
+        img: 'imgs/abilities/symmetra-e.png',
+    },
+    {
+        hero: 'symmetra',
+        key: 'q',
+        img: 'imgs/abilities/symmetra-q.png',
+    },
+    {
+        hero: 'symmetra',
+        key: 'shift',
+        img: 'imgs/abilities/symmetra-shift.png',
+    },
+    // Torbjorn
+    {
+        hero: 'torbjorn',
+        key: 'e',
+        img: 'imgs/abilities/torbjorn-e.png',
+    },
+    {
+        hero: 'torbjorn',
+        key: 'q',
+        img: 'imgs/abilities/torbjorn-q.png',
+    },
+    {
+        hero: 'torbjorn',
+        key: 'shift',
+        img: 'imgs/abilities/torbjorn-shift.png',
+    },
+    // Tracer
+    {
+        hero: 'tracer',
+        key: 'e',
+        img: 'imgs/abilities/tracer-e.png',
+    },
+    {
+        hero: 'tracer',
+        key: 'q',
+        img: 'imgs/abilities/tracer-q.png',
+    },
+    {
+        hero: 'tracer',
+        key: 'shift',
+        img: 'imgs/abilities/tracer-shift.png',
+    },
+    // Venture
+    {
+        hero: 'venture',
+        key: 'q',
+        img: 'imgs/abilities/venture-q.png',
+    },
+    {
+        hero: 'venture',
+        key: 'shift',
+        img: 'imgs/abilities/venture-shift.png',
+    },
+    {
+        hero: 'venture',
+        key: 'right-click',
+        img: 'imgs/abilities/venture-rc.png',
+    },
+    // Widowmaker
+    {
+        hero: 'widowmaker',
+        key: 'e',
+        img: 'imgs/abilities/widowmaker-e.png',
+    },
+    {
+        hero: 'widowmaker',
+        key: 'q',
+        img: 'imgs/abilities/widowmaker-q.png',
+    },
+    {
+        hero: 'widowmaker',
+        key: 'shift',
+        img: 'imgs/abilities/widowmaker-shift.png',
+    },
+    // Winston
+    {
+        hero: 'winston',
+        key: 'e',
+        img: 'imgs/abilities/winston-e.png',
+    },
+    {
+        hero: 'winston',
+        key: 'q',
+        img: 'imgs/abilities/winston-q.png',
+    },
+    {
+        hero: 'winston',
+        key: 'shift',
+        img: 'imgs/abilities/winston-shift.png',
+    },
+    // Wreckingball
+    {
+        hero: 'wreckingball',
+        key: 'e',
+        img: 'imgs/abilities/wreckingball-e.png',
+    },
+    {
+        hero: 'wreckingball',
+        key: 'q',
+        img: 'imgs/abilities/wreckingball-q.png',
+    },
+    {
+        hero: 'wreckingball',
+        key: 'shift',
+        img: 'imgs/abilities/wreckingball-shift.png',
+    },
+    {
+        hero: 'wreckingball',
+        key: 'right-click',
+        img: 'imgs/abilities/wreckingball-rc.png',
+    },
+    // Zarya
+    {
+        hero: 'zarya',
+        key: 'e',
+        img: 'imgs/abilities/zarya-e.png',
+    },
+    {
+        hero: 'zarya',
+        key: 'q',
+        img: 'imgs/abilities/zarya-q.png',
+    },
+    {
+        hero: 'zarya',
+        key: 'shift',
+        img: 'imgs/abilities/zarya-shift.png',
+    },
+    // Zenyatta
+    {
+        hero: 'zenyatta',
+        key: 'e',
+        img: 'imgs/abilities/zenyatta-e.png',
+    },
+    {
+        hero: 'zenyatta',
+        key: 'q',
+        img: 'imgs/abilities/zenyatta-q.png',
+    },
+    {
+        hero: 'zenyatta',
+        key: 'shift',
+        img: 'imgs/abilities/zenyatta-shift.png',
+    }
+]
