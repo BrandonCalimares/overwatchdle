4b48f7dc2c5063ce6814d61daf126dd0912a4f24
Brandon Calimares <brandoncalimares@gmail.com>
2024-09-25 18:39:28 -0300

    Cambiando lugar de boton de informacion y eliminando boton de idioma

--- js/main.js
+++ js/main.js
@@ -1,22 +1,6 @@
-const langModal = document.querySelector('.language-modal');
-const langBtn = document.querySelector('.language-btn');
-const closeLang = document.querySelector('.close-lang');
-
 const infoModal = document.querySelector('.info-modal');
 const infoBtn = document.querySelector('.info-btn');
 const closeInfo = document.querySelector('.close-info');
-
-/* Language modal methods */
-
-langBtn.addEventListener('click', () => {
-    langModal.showModal();
-})
-
-closeLang.addEventListener('click', () => {
-    langModal.close();
-})
-
-/* Info modal methods */
 
 infoBtn.addEventListener('click', () => {
     infoModal.showModal();
